7cf184fba98b7a187fcea6a6c979e7a29836b4b6
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-04-25 12:34:57 +0530

    Title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
   import "../app.css";
 </script>
 
+<svelte:head>
+    <title>URL Shortener</title>
+</svelte:head>
+
 <div class="flex flex-col min-h-screen justify-center items-center">
     <div class="flex flex-col min-h-screen justify-center items-center max-w-screen-md gap-4">
         <slot />
