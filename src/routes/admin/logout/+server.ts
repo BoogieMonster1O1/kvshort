f16c5e5e64604ec19792252042dc9e64859a92fe
Shrish Deshpande <shrish.deshpande@icloud.com>
2024-06-12 21:31:38 +0530

    Get method

--- src/routes/admin/logout/+server.ts
+++ src/routes/admin/logout/+server.ts
@@ -1,6 +1,6 @@
 import {error, redirect} from "@sveltejs/kit";
 
-export async function POST({ request, cookies }) {
+export async function GET({ request, cookies }) {
     cookies.set('api-key', undefined, { path: '/', httpOnly: true });
     redirect(302, '/admin')
 }
